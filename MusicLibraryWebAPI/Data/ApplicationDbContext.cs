80cc60294927f44aacf798dcb300358fede72789
gracekogithub <graceko02011972@gmail.com>
2021-03-30 10:40:28 -0700

    created music controller and updated database

--- MusicLibraryWebAPI/Data/ApplicationDbContext.cs
+++ MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,6 @@ namespace MusicLibraryWebAPI.Data
         {
 
         }
+        public DbSet<Music> Music;
     }
 }
